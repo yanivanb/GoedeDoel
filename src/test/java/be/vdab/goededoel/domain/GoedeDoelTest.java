1b078a7d21ca4257eb143c97b6a003989b6a1e72
Lightflamingo <Lightflamingowilreborn@gmail.com>
2022-11-01 12:56:50 +0100

    spotbug

--- src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 class GoedeDoelTest {
 	private static final String NAAM = "CliniClowns";
 	private GoedeDoel doel;
@@ -31,5 +33,9 @@ class GoedeDoelTest {
 	@Test
 	void doelenMetVerschillendeNaamZijnVerschillend() {
 		assertThat(doel).isNotEqualTo(new GoedeDoel("WWF"));
+	}
+	@Test
+	void doelVerschiltVanEenObjectMetEenAnderType() {
+		assertThat(doel).isNotEqualTo(BigDecimal.ZERO);
 	}
 }
