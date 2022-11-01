748084110a2ef1a66187973a9e3c99b60d2eca65
Lightflamingo <Lightflamingowilreborn@gmail.com>
2022-11-01 12:55:37 +0100

    jacoco

--- src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
@@ -23,4 +23,13 @@ class GoedeDoelTest {
 	void eenNieuwDoelHeeftNogGeenOpbrengst() {
 		assertThat(doel.getOpbrengst()).isZero();
 	}
+
+	@Test
+	void doelenMetDezelfdeNaamZijnGelijk() {
+		assertThat(doel).isEqualTo(new GoedeDoel(NAAM));
+	}
+	@Test
+	void doelenMetVerschillendeNaamZijnVerschillend() {
+		assertThat(doel).isNotEqualTo(new GoedeDoel("WWF"));
+	}
 }
